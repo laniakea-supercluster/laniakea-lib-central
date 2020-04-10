b5781a49fc4dafc76b1b251c26dbe91b82db1dce
adrianofsp <br.adriano.ferreira@gmail.com>
2020-04-10 09:47:41 -0300

    Added client auth.

--- src/domain/access.ts
+++ src/domain/access.ts
@@ -4,3 +4,7 @@ export * from './access/access-condition.enum';
 export * from './access/access-group.model';
 export * from './access/operator.model';
 export * from './access/platform.model';
+
+export * from './access/permission-type.enum';
+export * from './access/client.type';
+export * from './access/client.model';
