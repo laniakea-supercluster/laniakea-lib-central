382989c84cac7cba3f5dbbde75b16bb1d97f95e0
adrianofsp <br.adriano.ferreira@gmail.com>
2020-04-17 10:51:58 -0300

    Component model created.

---
+++ src/domain/access/component.model.ts
@@ -0,0 +1,15 @@
+import { IComponent, IModule } from './access.type';
+import { ObjectId } from 'mongodb';
+import { IApiPermission, ActionType } from '../access';
+
+// eslint-disable-next-line require-jsdoc
+export class Component implements IComponent {
+    name: string;
+    module: IModule;
+    apis: IApiPermission[];
+    permissions: ActionType[];
+    _id: string | ObjectId;
+    createdOn: Date;
+    changedOn: Date;
+    signature: string;
+}
